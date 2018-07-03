0ca0cad0cc0f3da61a8e54657519b860b21fc8b9
Cuan CLIFFORD <ccliffor@e4r25p4.wethinkcode.co.za>
2018-07-03 13:04:37 +0200

    gnl with 2d array

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccliffor <ccliffor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/05 10:55:27 by ccliffor          #+#    #+#             */
+/*   Updated: 2018/06/30 13:43:22 by ccliffor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 11000
+
+int	get_next_line(const int fd, char **line);
+
+#endif
